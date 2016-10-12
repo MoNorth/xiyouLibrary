2ab24096902f186c657a99d18a965d0c340f7c4e
e <573737092@qq.com>
2016-10-12 17:36:56 +0800

    bookInfo page done

--- js/getData.js
+++ js/getData.js
@@ -37,3 +37,59 @@ function getRank () {
 }
 
 getRank();
+
+
+//获取图书详情
+function getBookInfo(id, callback){
+	var $title = $('#bookInfoTitle');
+	var $content = $('#booInfoContent');
+	var moban = '<div valign="bottom" class="card-header color-white no-border no-padding">'+
+			    '<img class="card-cover" src="<%img%>" alt="">'+
+			    '</div><div class="card-content">'+
+			    '<div class="card-content-inner">'+
+			    '<p>作&nbsp;&nbsp;&nbsp;&nbsp;者：<%author%></p>'+
+			    '<p>出版社：<%pub%></p>'+
+			    '<p>分&nbsp;&nbsp;&nbsp;&nbsp;类：<%subject%></p>'+
+			    '<p>浏览次数：<%browseTimes%>次</p>'+
+			    '<p>收藏次数：<%favTimes%>次</p>'+
+			    '<p>可借数目：<%avaliable%>本</p>'+
+			    '</div></div>';
+
+	$.ajax({
+		type : 'get',
+		url : host + 'book/detail/id/' + id,
+		dataType : 'jsonp',
+		error : function(){
+			alert("失败了");
+		},
+		success : function(data){
+			if(!data || !data.Result || !data.Detail)
+			{
+				alert("数据加载失败");
+				return;
+			}
+
+			var detail = data.Detail
+
+			moban = moban.replace('<%author%>',detail['Author'] || "")
+						 .replace('<%pub%>',detail['Pub'] || "")
+						 .replace('<%subject%>',detail['Subject'] || "")
+						 .replace('<%browseTimes%>',detail['BrowseTimes'] || "")
+						 .replace('<%favTimes%>',detail['FavTimes'] || "")
+						 .replace('<%avaliable%>',detail['Avaliable'] || "");
+
+			if(detail['DoubanInfo'] && detail['DoubanInfo']['Images'])
+			{
+				var img = detail['DoubanInfo']['Images'];
+				moban = moban.replace('<%img%>',img['large'] || img['medium'] || img['small'] || "");
+			}else
+				moban = moban.replace('<%img%>',"");
+
+			$title.html(detail['Title']);
+			$content.html(moban);
+		}
+	});
+
+}
+
+getBookInfo('0100000015');
